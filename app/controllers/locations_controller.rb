b25484e3fcd862cb21700418302df3c2afc5610b
Frederico Araujo <fred.the.master@gmail.com>
2012-04-16 04:15:46 +0700

    pagination, updated rails and jquery

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,7 +2,7 @@ class LocationsController < ApplicationController
   # GET /locations
   # GET /locations.json
   def index
-    @locations = Location.all
+    @locations = Location.page(params[:page]).per(25)
 
     respond_to do |format|
       format.html # index.html.erb
